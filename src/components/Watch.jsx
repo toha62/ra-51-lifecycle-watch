215fdd6a6fe090ca05280fec172fd110a8582bca
Anton Trifonov <toha62@inbox.ru>
2023-04-27 14:19:33 +0300

    add analog watch

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -4,7 +4,7 @@ export default function Watch({ city, timeZone, handleClose }) {
   const getFormattedValue = (value) => {
     let strValue = value.toString();
     strValue = strValue.length === 2 ? strValue : '0' + strValue;
-    
+
     return strValue;
   }
 
@@ -44,7 +44,13 @@ export default function Watch({ city, timeZone, handleClose }) {
         <span key="3">{clock.seconds} </span>
       </div>   
       <div>
-        <button className="btn mb" onClick={handleClose}>close</button>
+        <button className="material-icons close-button" onClick={handleClose}>clear</button>
+      </div>
+      <div className="clock">
+        <div className="arrows seconds" style={{transform: `rotate(${(clock.seconds / 60) * 360 + 180}deg)`}}></div>
+        <div className="arrows minutes" style={{transform: `rotate(${(clock.minutes / 60) * 360 + 180}deg)`}}></div>
+        <div className="arrows hours" style={{transform: `rotate(${(clock.hours / 12) * 360 + 180}deg)`}}></div>
+        <div className="circle"></div>
       </div>
     </>      
   );
--- src/components/Watches.jsx
+++ src/components/Watches.jsx
@@ -5,7 +5,7 @@ export default function Watches({ watchList, handleClose }) {
   return (
     <ul className="list-group list-group-horizontal">      
       {watchList.map((watch) =>
-        <li key={watch.key} className="list-group-item">
+        <li key={watch.key} className="list-group-item m-3">
           <Watch
             city={watch.city}
             timeZone={watch.timeZone}            
